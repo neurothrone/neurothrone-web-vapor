b45a1a231cfd0921bcbf8a27e299e87b87daddb4
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-01-18 10:02:06 +0100

    remove comments and unused route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,14 +2,6 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-  //    app.get { req async throws in
-  //        try await req.view.render("index", ["title": "Hello Vapor!"])
-  //    }
-  
-  app.get("hello") { req async -> String in
-    "Hello, world!"
-  }
-  
   //    try app.register(collection: TodoController())
   try app.register(collection: WebsiteController())
   try app.register(collection: WebsiteProjectsController())
